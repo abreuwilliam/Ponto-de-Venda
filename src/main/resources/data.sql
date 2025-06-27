9defa13d13573e41b646f948dac9c723ae1e2d22
William Abreu <abreuwilliam>
2025-06-27 00:11:44 -0300

    ajuste

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 -- Inserir usuário admin
+System.out.println("Rodando data.sql");
 INSERT INTO usuario (id, username, password, role)
 VALUES (1, 'william_admin', '$2a$12$jeTsXeTVrNwhFyxzvTY52OEBCmgmkW7/UC2sqIzXYGSMhHUsYHuK.', 'ADMIN');
 
